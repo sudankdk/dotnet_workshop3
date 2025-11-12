e88d32f19390797882002559ccb7bbc76bee2844
sudankdk <sudankhadka345@gmail.com>
2025-11-12 11:39:45 +0545

    task 1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Operators operators = new Operators();
+        int ans = operators.Add(2, 3);
+        int subs = operators.Subtract(5, 2);
+        int product =operators.Multiply(5, 2);
+       var  ( val , msg)=operators.Divide(5, 0);
+        var oddEven = operators.OddEvenFinder(14);
+        Console.WriteLine(oddEven);
+        Console.WriteLine(ans);
+        Console.WriteLine(product);
+        Console.WriteLine($"{msg}: {val}");
+        Console.WriteLine(subs);
+        Console.WriteLine(ans);
     }
 }
