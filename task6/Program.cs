cacb72d8c1a88a5f2e751dcde3bef091f2f81218
sudankdk <sudankhadka345@gmail.com>
2025-11-12 20:05:16 +0545

    task 6 done

---
+++ task6/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter number: ");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine($"You entered: {num}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number format");
+            }
+            finally
+            {
+                Console.WriteLine("Program Executed");
+            }
+
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            try
+            {
+                if (password.Length < 6)
+                {
+                    throw new Exception("Password must be at least 6 characters long.");
+                }
+
+                Console.WriteLine("Password accepted.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
